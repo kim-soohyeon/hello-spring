1b8ac25a9060fb4380665b57c530347fdb8c63d4
kim-soohyeon <kimsh1691@gmail.com>
2025-03-27 15:44:10 +0900

    feat: 회원 등록 폼 추가

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello.hello_spring.controller;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -12,6 +13,11 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
     }
 
 }
